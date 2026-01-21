f5684472d44504f225e9a7f7f0700bc9b3a2b958
ivoaraujocbm <ivo.araujo@cbminformatica.com.br>
2026-01-21 15:26:39 -0300

    documentação adicionada a aplicação

---
+++ src/components/ClienteForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>{{ cliente ? 'Editar Cliente' : 'Novo Cliente' }}</h2>
+
+    <input v-model="form.nome" placeholder="Nome"/>
+    <input v-model="form.email" placeholder="Email"/>
+
+    <button @click="salvar">Salvar</button>
+    <button @click="$emit('cancelar')">Cancelar</button>
+  </div>
+</template>
+
+<script setup>
+  import { reactive, watch } from 'vue';
+
+  const props = defineProps({
+    cliente: Object
+  })
+
+  const emit = defineEmits(['salvar', 'cancelar'])
+
+  const form = reactive({
+    id: null, 
+    nome: '', 
+    email: ''
+  })
+
+  watch(
+  )
+
+</script>
